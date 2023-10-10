6d0c5a10defa454b7b8be75beba0c55ceac9dd6f
JuanMaH24 <j.herrera5@utp.edu.co>
2023-10-10 14:15:22 -0500

    Test Antibioticos

--- Modelo/Antibioticos.py
+++ Modelo/Antibioticos.py
@@ -1,12 +1,14 @@
 class Antibioticos():
   
     def __init__(self, nombre_producto, dosis_antibiotico, tipo_animal, valor_producto):
+        if nombre_producto== "" or dosis_antibiotico== "" or tipo_animal== "" or valor_producto== "":
+            raise(ValueError("Debes ingresar todos los campos"))
         if dosis_antibiotico >= 400 and dosis_antibiotico >= 600:
-            raise ValueError("La dosis debe estar entre 400 y 600.")
+            raise (ValueError("La dosis debe estar entre 400 y 600."))
         if tipo_animal.upper() not in ["BOVINO", "PORCINO", "CAPRINO"]:
-            raise ValueError("El tipo de animal no es válido.")
+            raise (ValueError("El tipo de animal no es válido."))
         if valor_producto < 0:
-            raise ValueError("El valor no puede ser negativo.")
+            raise (ValueError("El valor no puede ser negativo."))
         self.__nombre_producto = nombre_producto
         self.__dosis_antibiotico = dosis_antibiotico
         self.__tipo_animal = tipo_animal
