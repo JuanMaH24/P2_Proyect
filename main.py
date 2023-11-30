c265cf4bb6c93cbb89aa8aac3ef300452f871a6f
sofia <sofiasoto0281@gmail.com>
2023-11-29 22:08:34 -0500

    Cambio en carpetas y creancion del entorno.By Sofi :)

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-"""" 
-Acccion: Crear factura
-1. pedimos ingresar la cedula del cliente
-2. buscar cedula
-3 Si existe el cliente
-  3.1  Crear una factura CRUD
-  3.2  Asociar factura al cliente CRUD
- 
-  Si no existe el cliente
-
-  3.1 Crear un cliente CRUD
-  3.2  Cr6ear una factura CRUD
-  3.3  Asociar factura al cliente CRUD
-
-"""
 import sys
 import os
 myDir = os.getcwd()
